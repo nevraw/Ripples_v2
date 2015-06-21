2c450abf240ea7d5b049a2f580ad97a14336c4d1
nevraw <nevraw@users.noreply.github.com>
2015-06-21 17:43:15 +0100

    Update Ripples_config_col2.js

--- Ripples_config_col2.js
+++ Ripples_config_col2.js
@@ -116,7 +116,7 @@ $(document).ready(function () {
     }
 
 
-    $(":radio[value=" + settings.background + "]").attr('checked', true);
+//    $(":radio[value=" + settings.background + "]").attr('checked', true);
 
     $(".number").css("color", '#' + GColor.toHex(settings.timecolor));
     $("#timecolor").spectrum("set", '#' + GColor.toHex(settings.timecolor));
